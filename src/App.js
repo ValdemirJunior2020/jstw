7358cc231976f266db5f20b3746d1b29cb1abc5e
ValdemirJunior2020 <infojr.83@gmail.com>
2024-05-20 14:03:41 -0400

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('JSTW');
+
+    const changeMessage = () => {
+        setMessage("Digital Solutions");
+    };
+
+    return (
+        <div className="App">
+            <Navbar />
+            <header className="App-header">
+                <h1 className="futuristic">{message}</h1>
+                <button onClick={changeMessage}>Change Message</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
